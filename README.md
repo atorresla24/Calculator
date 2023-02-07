56b6654c059ca316da3f773107784ff00ca04043
Alec Torres <atorresla24@gmail.com>
2023-02-07 15:59:52 -0500

    added tailwind and header

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Calculator
 
 # Day 1: Laying down groundwork for site
+
+# Day 2: Added Tailwind CSS
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./main_site/home.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
